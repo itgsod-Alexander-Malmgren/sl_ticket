9221aa0e613bd58e4835e2acace9889a058e14a5
alexander.malmgren1 <alexander.malmgren2@elev.it-gymnasiet.se>
2017-10-11 11:48:28 +0200

    Sl ticket

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,15 @@
 def fares(age, student=False, senior=False):
     print age,student,senior
-    return 'hel'
+    if age <=20:
+        return 'halv'
+    elif age >=65:
+        return 'halv'
+    else:
+        return'hel'
 
 
 
-
-
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+print 'barn 11',fares(11)
+print 'pension 63',fares(63)
+print 'student 25'fares(25,student=True)
+print 'vuxen 45',fares(45)
