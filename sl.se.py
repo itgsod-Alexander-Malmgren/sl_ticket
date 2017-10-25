c1328c458085ae5f80f927809493950d8654964c
alexander.malmgren1 <alexander.malmgren2@elev.it-gymnasiet.se>
2017-10-25 12:33:38 +0200

    SL

--- sl.se.py
+++ sl.se.py
@@ -11,5 +11,5 @@ def fares(age, student=False, senior=False):
 
 print 'barn 11',fares(11)
 print 'pension 63',fares(63)
-print 'student 25'fares(25,student=True)
+print 'student 25', fares(25,student=True)
 print 'vuxen 45',fares(45)
